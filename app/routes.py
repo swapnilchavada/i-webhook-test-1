80874f474639e4d9eba01d71f708ad411e2e61b3
swapnilchavada <30380804+swapnilchavada@users.noreply.github.com>
2021-05-13 00:39:51 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,39 +5,131 @@ import json
 import requests
 
 WEBHOOK_VERIFY_TOKEN = 'test_faq_token'
-PAGE_ACCESS_TOKEN = 'EAAeeVSYcYQYBAHng9dsVAiRdvy1kKttTvWFkcmoU2fwUr44vnJXVQJfXzuZCL8FAUM9IqZCqlZBgaWMbRw6JQ09xlF9s130Syzt1trfbZCC1jxbM4IHtPqxVXRg6DB11rsDheteFCtLuXHqJWTXqHkz4gMV7QZBRATgucFmF2ywZDZD'
-#this is for page Page_thread_queue_test
-PAGE_ACCESS_TOKEN2 = 'EAAeeVSYcYQYBAOVAMY6T8t6htQMnJ3gGZBfq9H7VsvRaazNsqJ6FIfsIYK2GBAWRNrFzyB95BADbeZBZClDP6Vdf7Jp7gtpYIIur4oPZCBl4VXpAf3P4kjM8ldR3heOXbUZCFBzvk6rfB0iTPOlqhSKUySb8afVp8rLNnBMR1O0E71D1vfOlvAu3g0lvGRAkZD'
-
-SEND_API_URL = 'https://graph.facebook.com/v2.12/me/messages?access_token=%s'\
-  % PAGE_ACCESS_TOKEN
-SEND_API_URL2 = 'https://graph.facebook.com/v2.12/me/messages?access_token=%s'\
-  % PAGE_ACCESS_TOKEN2
-
-PASS_THREAD_CONTROL_URL = 'https://graph.facebook.com/v2.12/me/pass_thread_control?access_token=%s'\
-  % PAGE_ACCESS_TOKEN
-
-TAKE_THREAD_CONTROL_URL = 'https://graph.facebook.com/v2.12/me/take_thread_control?access_token=%s'\
-% PAGE_ACCESS_TOKEN
+PAGE_ACCESS_TOKEN = 'EAAGFZAiGnU9YBAAoLhyL2gDlvOSLp1PaQUWEFJtRjZBQC1zYpUpDVKbqLJNzkcueTcZChoRGQ66uCgFZBYrdL8cLAJV9hscj3pMfrhMMZAr1gZCJA2nFQ1KqitcAb0CfL6GZCYkLmmhVIZBRC4oIl21JdtjJyxtzsBJwpKweOHIiCIZCsqNMHrlZCI'
+SEND_API_URL = 'https://graph.facebook.com/v5.0/me/messages?access_token=%s'\
+  %PAGE_ACCESS_TOKEN
 
 HEADERS = {'content-type': 'application/json'}
-
-PAGE_INBOX = 263902037430900
-
-ME = '620697518375534'
-
+IG_ACC_TO_REPLY = '90010132720480'
+      
+      
 def send_message(body):
-  print('send_message')
-  print(body)
   try:
-    #send_message_to_recipient(json.dumps(body), sender, recipient_id)
-    print('sender')
-    #print(sender)
+    for entry in body['entry']:
+      if(entry['id'] != IG_ACC_TO_REPLY):
+        return
+      for message in entry['messaging']:
+        sender = message['sender']['id']
+        recipient_id =  message['recipient']['id']
+        if 'message' in message: 
+          webhook_type='message'
+        elif 'postback' in message: 
+          webhook_type='postback'
+        else:
+          return
+        if 'text' in message[webhook_type]:
+          msg_text = message[webhook_type]['text']
+          if 'echoing_back' in msg_text:
+            return
+          elif 'qr' in msg_text:
+            send_message_with_qr_to_recipient(sender, recipient_id)
+            return
+        body['echoing_back'] = 'true'
+        if 'is_echo' in message[webhook_type]:
+          send_message_to_recipient(json.dumps(body), recipient_id, sender)
+          print('sent message to', recipient_id)
+        else:
+          send_message_to_recipient(json.dumps(body), sender, recipient_id)
+          print('sent message to', sender)
   except Exception as e:
      print("swapnilc-Exception sending")
      print(e)
       
-      
+def send_message_with_qr_to_recipient(recipient_id, page_id):
+  message = {
+    'recipient': {
+      'id': recipient_id,
+    },
+    'message': {
+    "text": "Pick a color echoing_back :",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Red",
+        "payload":"red-payload",
+      },
+      {
+        "content_type":"text",
+        "title":"Green",
+        "payload":"green-payload",
+     },
+     {
+        "content_type":"text",
+        "title":"Purple",
+        "payload":"purple-payload",
+     },
+     {
+        "content_type":"text",
+        "title":"Yellow",
+        "payload":"yellow-payload",
+     },
+      {
+        "content_type":"text",
+        "title":"Blue",
+        "payload":"blue-payload",
+     },
+     {
+        "content_type":"text",
+        "title":"Max 20 chars-Long Long Text for QR Testing",
+        "payload":"red-payload",
+      },
+      {
+        "content_type":"text",
+        "title":"Max 20 chars-Long Long Text for QR Testing",
+        "payload":"green-payload",
+     },
+     {
+        "content_type":"text",
+        "title":"Max 20 chars-Long Long Text for QR Testing",
+        "payload":"green-payload",
+     },
+      {
+        "content_type":"text",
+        "title":"Max 20 chars-Long Long Text for QR Testing",
+        "payload":"yellow-payload",
+     },
+      {
+        "content_type":"text",
+        "title":"Max 20 chars-Long Long Text for QR Testing",
+        "payload":"blue-payload",
+     },
+     {
+        "content_type":"text",
+        "title":"Max 20 chars-Long Long Text for QR Testing",
+        "payload":"red-payload",
+      },
+      {
+        "content_type":"text",
+        "title":"Max 20 chars-Long Long Text for QR Testing",
+        "payload":"green-payload",
+     },
+     {
+        "content_type":"text",
+        "title":"Max 20 chars-Long Long Text for QR Testing",
+        "payload":"green-payload",
+     }
+    ]
+  },
+    'tag': 'human_agent',
+  }
+  r = requests.post(SEND_API_URL, data=json.dumps(message), headers=HEADERS)
+  if r.status_code != 200:
+    print('== ERROR====')
+    print(SEND_API_URL)
+    print(r.json())
+    print('==============')
+
+    
 def send_message_to_recipient(message_text, recipient_id, page_id):
   message = {
     'recipient': {
@@ -46,13 +138,15 @@ def send_message_to_recipient(message_text, recipient_id, page_id):
     'message': {
       'text': message_text,
     },
+    'tag': 'human_agent',
   }
-  r = requests.post(SEND_API_URL if page_id == '620697518375534' else SEND_API_URL2, data=json.dumps(message), headers=HEADERS)
+  r = requests.post(SEND_API_URL, data=json.dumps(message), headers=HEADERS)
   if r.status_code != 200:
     print('== ERROR====')
     print(SEND_API_URL)
     print(r.json())
     print('==============')
+
 
 @app.route('/')
 @app.route('/index')
